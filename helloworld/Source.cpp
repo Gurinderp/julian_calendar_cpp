3bca967cf48035fae0799a987771562c4167680a
rednirug <sgurinderpreet@gmail.com>
2021-02-22 21:27:37 -0800

    removed a b c d from main() and localized them to process()

--- helloworld/Source.cpp
+++ helloworld/Source.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 
 void input(int& month, int& day, int& year);
-void process(int month, int day, int year, 
-	int& a, int& b, int& c, 
-	int& d, int& jdn, float& dow);
-void output(int month, int day, int year, 
-	int jdn, int a, int b, int c, int d, float dow);
+void process(int month, int day, int year, int& jdn);
+void output(int month, int day, int year, int jdn);
 
 int main()
 {
-	int month;
-	int day;
-	int year;
-	int a;
-	int b;
-	int c;
-	int d;
-	int jdn;
-	float dow;
+	int month; // Month input by user
+	int day; // Day input by user
+	int year; // Year input by user
+	int jdn; // JDN calculation output by process module
 
 	input(month, day, year);
-	process(month, day, year, a, b, c, d, jdn, dow);
-	output(month, day, year, jdn, a, b, c, d, dow);
+	process(month, day, year, jdn);
+	output(month, day, year, jdn);
 }
 
 // Input module that gathers input from the user to
 // provide data needed for process module calculations
-void input(int& month, int& day, int& year) {
+void input(int& month, int& day, int& year) 
+{
 	// Asks for month
 	std::cout << "Enter Month: ";
 	std::cin >> month;
@@ -40,29 +33,27 @@ void input(int& month, int& day, int& year) {
 
 // Process module that takes in month, day, and year to 
 // calculate each step
-void process(int month, int day, int year, 
-	int& a, int& b, int& c, 
-	int& d, int& jdn, float& dow)
+void process(int month, int day, int year, int& jdn)
 {
-	a = (14 - month) / 12;
-	b = (month - 3) + (12 * a);
-	c = 4800 + year - a;
-	d = (c / 4) - (c / 100) + (c / 400);
+	// First step calculation
+	int a = (14 - month) / 12;
+	// Second step calculation
+	int b = (month - 3) + (12 * a);
+	// Third step calculation
+	int c = 4800 + year - a;
+	// Fourth step calculation
+	int d = (c / 4) - (c / 100) + (c / 400);
+	// Final calculation for JDN
 	jdn = day + (((153 * b) + 2) / 5) + (365 * c) + d - 32045;
-	dow = (jdn + 1) % 7;
 }
 
 // Outputs calculations by process function showing 
 // the results of a, b, c, d, DOW, and JDN
-void output(int month, int day, int year, 
-	int jdn, int a, int b, int c, int d, float dow) {
-	std::cout << "The JDN for " << month 
-		<< "/" << day 
-		<< "/" << year 
-		<< "is: " << jdn << std::endl
-		<< "DOW: " << dow << std::endl
-		<< "a: " << a << std::endl
-		<< "b: " << b << std::endl
-		<< "c: " << c << std::endl
-		<< "d: " << d << std::endl;
+void output(int month, int day, int year, int jdn) 
+{
+	std::cout << "The JDN for " << month
+		<< "/" << day
+		<< "/" << year
+		<< " is: " << jdn 
+		<< std::endl;
 }
