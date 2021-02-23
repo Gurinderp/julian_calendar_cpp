5dbbadf7019af091c91214f32d444801f845a1c2
rednirug <sgurinderpreet@gmail.com>
2021-02-22 21:08:46 -0800

    comments added and output alteration

--- helloworld/Source.cpp
+++ helloworld/Source.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 void input(int& month, int& day, int& year);
-void process(int month, int day, int year, int& a, int& b, int& c, int& d, int& jdn, float& dow);
-void output(int jdn, int a, int b, int c, int d, float dow);
+void process(int month, int day, int year, 
+	int& a, int& b, int& c, 
+	int& d, int& jdn, float& dow);
+void output(int month, int day, int year, 
+	int jdn, int a, int b, int c, int d, float dow);
 
 int main()
 {
@@ -18,19 +21,28 @@ int main()
 
 	input(month, day, year);
 	process(month, day, year, a, b, c, d, jdn, dow);
-	output(jdn, a, b, c, d, dow);
+	output(month, day, year, jdn, a, b, c, d, dow);
 }
 
+// Input module that gathers input from the user to
+// provide data needed for process module calculations
 void input(int& month, int& day, int& year) {
+	// Asks for month
 	std::cout << "Enter Month: ";
 	std::cin >> month;
+	// Asks for day
 	std::cout << "Enter Day: ";
 	std::cin >> day;
+	// Asks for year
 	std::cout << "Enter Year: ";
 	std::cin >> year;
 }
 
-void process(int month, int day, int year, int& a, int& b, int& c, int& d, int& jdn, float& dow)
+// Process module that takes in month, day, and year to 
+// calculate each step
+void process(int month, int day, int year, 
+	int& a, int& b, int& c, 
+	int& d, int& jdn, float& dow)
 {
 	a = (14 - month) / 12;
 	b = (month - 3) + (12 * a);
@@ -40,8 +52,14 @@ void process(int month, int day, int year, int& a, int& b, int& c, int& d, int& 
 	dow = (jdn + 1) % 7;
 }
 
-void output(int jdn, int a, int b, int c, int d, float dow) {
-	std::cout << "JDN: " << jdn << std::endl
+// Outputs calculations by process function showing 
+// the results of a, b, c, d, DOW, and JDN
+void output(int month, int day, int year, 
+	int jdn, int a, int b, int c, int d, float dow) {
+	std::cout << "The JDN for " << month 
+		<< "/" << day 
+		<< "/" << year 
+		<< "is: " << jdn << std::endl
 		<< "DOW: " << dow << std::endl
 		<< "a: " << a << std::endl
 		<< "b: " << b << std::endl
