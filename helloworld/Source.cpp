ddb5eaef6071353ad1beb614c5dc15cba739aa98
rednirug <sgurinderpreet@gmail.com>
2021-03-01 22:21:30 -0800

    comments added

--- helloworld/Source.cpp
+++ helloworld/Source.cpp
@@ -12,13 +12,19 @@ void display_results(int today_year, int today_month, int today_day, int today_j
 
 int main()
 {
-	int today_year, today_month, today_day, today_jdn; // 
-	int birthday_year, birthday_month, birthday_day, birthday_jdn; // 
-	int birthday_dow, days_between; // 
+	// Delcared today_year, today_month, today_day, and today_jdn as Integers
+	int today_year, today_month, today_day, today_jdn;
+	// Declared birthday_year, birthday_month, birthday_day, and birthday_jdn
+	// as Integers
+	int birthday_year, birthday_month, birthday_day, birthday_jdn; 
+	// Declared birthday_dow and days_between as Integers
+	int birthday_dow, days_between; 
 	
+	// Prompts user to input todays month, day, and year
 	std::cout << "For today's date...";
 	get_date(today_year, today_month, today_day);
 	
+	// Prompts user to input the month, day, and year of their next birthday
 	std::cout << "For your next birthday...";
 	get_date(birthday_year, birthday_month, birthday_day);
 	compute_jdn(today_year, today_month, today_day, today_jdn);
@@ -30,7 +36,7 @@ int main()
 		days_between);
 }
 
-
+// Gathers input provided by user for current date
 void get_date(int& year, int& month, int& day) 
 {
 	std::cout << "Enter month number (Jan=1, Feb=2, etc.): ";
@@ -41,6 +47,7 @@ void get_date(int& year, int& month, int& day)
 	std::cin >> year;
 };
 
+// Processes the year, month, and day to compute the JDN
 void compute_jdn(int year, int month, int day, int& jdn)
 {
 	int a, b, c, d;
@@ -51,6 +58,7 @@ void compute_jdn(int year, int month, int day, int& jdn)
 	jdn = day + (((153 * b) + 2) / 5) + (365 * c) + d - 32045;
 }
 
+// Processes year, month, and day to compute the DOW
 void compute_dow(int year, int month, int day, int& dow)
 {
 	int jdn;
@@ -58,11 +66,14 @@ void compute_dow(int year, int month, int day, int& dow)
 	dow = (jdn + 1) % 7;
 }
 
+// Processes the current date JDN and the next birthday JDN to
+// compute the difference
 void compute_days(int jdn1, int jdn2, int& days_between)
 {
 	days_between = jdn2 - jdn1;
 }
 
+// Displays date in [MM-DD-YYYY] format
 void display_date(int year, int month, int day)
 {
 	std::cout << month
@@ -72,6 +83,8 @@ void display_date(int year, int month, int day)
 		<< year;
 }
 
+// Outputs the results for the current dates JDN, the next birthday JDN,
+// and next birthday DOW, and the days remaining until the next birthday
 void display_results(int today_year, int today_month, int today_day,
 	int today_jdn, int birthday_year, int birthday_month, int birthday_day,
 	int birthday_jdn, int birthday_dow, int days_between)
